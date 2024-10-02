4e52bf4519300d66706bba1d2ce93767095c1b7a
M a d u h <88396163+Madhyy@users.noreply.github.com>
2024-10-02 00:30:07 -0300

    asdffg

--- js/perguntas.js
+++ js/perguntas.js
@@ -16,7 +16,7 @@ export const perguntas = [
                     "Os dois mendigos duelaram até o fim, menos um mendigo faminto no mundo e mais um satisfeito após comer o pão.",
                     "Após ir embora os mendingos continuaram até que se cansaram e dividiram o pão voluntariamente."
                 ],
-                proxima: 2,
+                proxima: 1,
             },
         ]
     },
@@ -30,7 +30,7 @@ export const perguntas = [
                     "Fazendo Parkour nas casinhas do bairro esquecido, você pisa numa telha solta e cai dentro da casinha. Lá estava o homem do qual você chama de pai com sua segunda familia, após o choque você recebe alguns trocados para se calar, dinheiro que pode ser usado para investir em organizações de cunho social.",
                     "Ante mesmo de conseguir correr para se desviar da água, devido a escuridão e a rua com buracos, você tropeça e cai, deixando cair alguns trocados que em breve alguém que vive ali pode encontrar, abrindo portas para um futuro brihante a essa pessoa que pode usar o dinheiro para melhorar de vida ou então gastar com pinga."
                 ],
-                proxima: 3,
+                proxima: 1,
             },
             {
                 texto: "Reportar a situação para o prefeito.",
@@ -39,7 +39,7 @@ export const perguntas = [
                     "Após reportar de maneira sensata a situação ao prefeito, o bairro passa por uma transforção onde todos os moradores são retirados de suas casa e por fim apartamentos são feitos para acomodar mais pessoas, mas eles não foram feito exatamente para os antigos moradores viverem (parabéns, acabou com familias)",
                     "Depois de reportar, o prefeito concordou em restaurar o bairro e agora além de estar iluminado e asfaltado, o bairro tem um nome."
                 ],
-                proxima: 4,
+                proxima: 1,
             },
         ]
     },
@@ -53,7 +53,7 @@ export const perguntas = [
                     "Era um movimento promovendo ações para ajudar aspirantes a politicos, mas então você se recorda do atual prefeito e do bairro esquecido, foi assim que o prefeito ocupou o atual cargo, com isso você desvia as arrecadações desse grupos e investe.",
                     "Se juntando ao grupo que promovia ações você se sente realizado, mas falha ao perceber que o grupo ajudava tiktokers a crescer, apoiavam npc's e diagnosticava gratuitamente outras pessoas com base em videos do tiktok (já pode terminar aqui o desafio e ir pro tiktok)."
                 ],
-                proxima: 4,
+                proxima: 1,
             },
             {
                 texto: "Paro para observar e vou embora.",
@@ -62,7 +62,7 @@ export const perguntas = [
                     "É um grupo que promovia ações para arrecadar fundos para manter os próximos CBLOL, estavam infelizes pois acabou, inspirado por você não ser um deles você vai para casa feliz já que você tem emprego e colabora com ONG's ",
                     "Na verdade era apenas a Virginia e companhia tentando vender rifa após o tigrinho levar suas propriedades"
                 ],
-                proxima: 3,
+                proxima: 1,
             },
         ]
     },
